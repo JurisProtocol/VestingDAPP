bbd0bf4ce05002609c84686d11077239475ab06b
Axylai Frankland <140928596+OtterConsultant@users.noreply.github.com>
2024-10-06 11:54:35 -0400

    added php files

---
+++ php/validator.php
@@ -0,0 +1,30 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+$api_url = "https://api.jurisprotocol.com/api/airdrop/eligibility_data";
+
+// Initialize a cURL session
+$ch = curl_init();
+
+// Set the cURL options
+curl_setopt($ch, CURLOPT_URL, $api_url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+
+// Execute the request
+$response = curl_exec($ch);
+
+// Check for errors
+if(curl_errno($ch)) {
+    echo 'Error: ' . curl_error($ch);
+} else {
+    // Set the appropriate headers
+    header('Content-Type: ' . curl_getinfo($ch, CURLINFO_CONTENT_TYPE));
+    echo $response;
+}
+
+// Close the cURL session
+curl_close($ch);
+?>
